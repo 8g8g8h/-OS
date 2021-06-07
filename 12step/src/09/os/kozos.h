0d805aad9f0b3dfa6dfb5d1d657596b75f8888ec
YOSHIDA YUKI <yuki-yoshida@isl.im.dendai.ac.jp>
2021-06-07 21:44:45 +0900

    add file

---
+++ 12step/src/09/os/kozos.h
@@ -0,0 +1,42 @@
+#ifndef _KOZOS_H_INCLUDED_
+#define _KOZOS_H_INCLUDED_
+
+#include "defines.h"
+#include "syscall.h"
+
+
+/*システムコール*/
+/*スレッドの起動のシステムコール*/
+kz_thread_id_t kz_run(kz_func_t func,char *name,int prioprity,int stacksize,int argc,char *argv[]);
+
+/*スレッドの終了のシステムコール*/
+void kz_exit(void);
+int kz_wait(void);
+int kz_sleep(void);
+int kz_wakeup(kz_thread_id_t id);
+kz_thread_id_t kz_getid(void);
+int kz_chpri(int priority);
+
+/*ライブラリ関数*/
+
+/*初期スレッドを起動し、OSの動作を開始*/
+void kz_start(kz_func_t func,char *name,int priority,int stacksize,int argc,char *argv[]);
+
+/*エラー表示*/
+void kz_sysdown(void);
+
+/*システムコールを実行*/
+void kz_syscall(kz_syscall_type_t type,kz_syscall_param_t *param);
+
+/*ユーザースレッド*/
+int test09_1_main(int argc,char *argv[]);
+int test09_2_main(int argc,char *argv[]);
+int test09_3_main(int argc,char *argv[]);
+extern kz_thread_id_t test09_1_id;
+extern kz_thread_id_t test09_2_id;
+extern kz_thread_id_t test09_3_id;
+
+
+
+
+#endif
